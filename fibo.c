814bb007c24055996a4bc82b369de13455527563
tandandanw <tandandanw@gmail.com>
2020-10-03 11:01:41 +0800

    Add binet formula implementation

--- fibo.c
+++ fibo.c
@@ -1,20 +1,22 @@
 //--------------------------------------------------------------------------------
-// fibo_v0: create empty framework
+// fibo-1_v1: add binet formula implementation
 // Copyright (c) tandandan
-//--------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
 
 #include <stdio.h>
+#include <math.h>
 
 int fibonacci(int n)
 {
-    return -1;
+    // Binet formula
+    double golden_ratio = (1 + sqrt(5)) / 2;
+    return round(pow(golden_ratio, n) / sqrt(5));
 }
 
 int main(int args, char *argv[])
 {
-    int n; scanf("%d", &n);
-    int result = fibonacci(n);
-    printf("%d", result);
-
+    int testdata; scanf("%d", &testdata);
+    int result = fibonacci(testdata);
+    printf("f(%d) = %d\n", testdata, result);
     return 0;
 }
