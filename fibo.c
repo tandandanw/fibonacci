d7afacfaaa95b454f564bc07f140a266eb36a358
tandandanw <tandandanw@gmail.com>
2020-10-03 11:04:43 +0800

    Redirect stdin to file

--- fibo.c
+++ fibo.c
@@ -1,22 +1,33 @@
 //--------------------------------------------------------------------------------
-// fibo-1_v1: add binet formula implementation
+// fibo-1_v2: redirect stdin to file
 // Copyright (c) tandandan
 //-------------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <math.h>
 
-int fibonacci(int n)
-{
+typedef unsigned long long llu;
+
+ llu fibonacci(int n)
+ {
     // Binet formula
     double golden_ratio = (1 + sqrt(5)) / 2;
     return round(pow(golden_ratio, n) / sqrt(5));
-}
+ }
 
 int main(int args, char *argv[])
 {
-    int testdata; scanf("%d", &testdata);
-    int result = fibonacci(testdata);
-    printf("f(%d) = %d\n", testdata, result);
+    if (args > 1) freopen(argv[1], "r", stdin);
+
+    int testdata_num;
+    scanf("%d", &testdata_num);
+    while (testdata_num--)
+    {
+        int testdata; scanf("%d", &testdata);
+        double seconds = 0;
+        llu result = fibonacci(testdata);
+        printf("f(%d) = %llu\n", testdata, result);
+    }
+
     return 0;
 }
